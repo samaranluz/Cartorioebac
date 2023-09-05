970b2aa2d8c447b450bed6986bd2a51c5abe3f3f
samaranluz <143106574+samaranluz@users.noreply.github.com>
2023-09-05 20:47:42 -0300

    Correção da tela inicial
    Inclusão do 4 - Sair do sistema

--- Cartorio_sete.c
+++ Cartorio_sete.c
@@ -133,7 +133,8 @@ int main()
 	 printf("Escolha a opção deseja no menu:\n\n");
 	 printf("\t1 - Registrar nomes\n");
 	 printf("\t2 - Consultar nomes\n");
-	 printf("\t3 - Deletar nomes\n\n");// fim do menu
+	 printf("\t3 - Deletar nomes\n");
+	 printf("\t4 - Sair do sistema\n\n"); // fim do menu 
   
       scanf("%d" , &opcao); // armazenando a escolha do usuário
 	               	   
